8d8b9b7d0d47689d55c7fbaf27c2e5f44a463d46
basher <dev.teeraphat@gmail.com>
2023-01-27 11:15:32 +0700

    ✨ feat | seq import

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,17 @@
 import helmet from 'helmet';
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { Logger, ValidationPipe, VersioningType } from '@nestjs/common';
 
 import { AppModule } from 'src/app.module';
 import { NestConfig } from '@config/configuration.interface';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
   const nestConfig = configService.get<NestConfig>('nest');
-  const swaggerConfig = new DocumentBuilder()
-    .setTitle('boilerplate')
-    .setDescription('The boilerplate API description')
-    .setVersion('1')
-    .build();
 
   app.use(helmet());
 
@@ -34,10 +29,16 @@ async function bootstrap() {
 
   // TODO: Exception
 
-  const port = nestConfig?.port || 3001;
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('boilerplate')
+    .setDescription('The boilerplate API description')
+    .setVersion('1')
+    .build();
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('docs', app, document);
+
+  const port = nestConfig?.port || 3001;
 
   await app.listen(port);
   Logger.log(`Started application in NODE_ENV=${nestConfig?.env} on port ${port}`);
